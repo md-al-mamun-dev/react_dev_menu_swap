5a374bc3e91435fee90474d93b0c3d39fa1ec2f6
MD.AL MAMUN <mamunofficialmail@gmail.com>
2025-04-27 08:15:03 +0600

    ...

---
+++ script.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector('nav');
+const menu = nav.firstElementChild.children[2].children[0];
+const communityElement = menu.querySelector('div a[href="/community"]').closest('div')
+const referenceElement = menu.querySelector('div a[href="/reference/react"]').closest('div')
+
+const children = Array.from(menu.children);
+
+const communityIndex = children.indexOf(communityElement);
+const referenceIndex = children.indexOf(referenceElement);
+
+const swapSessionData = JSON.parse(sessionStorage.getItem('swapSession'));
+
+if(swapSessionData){
+    if(swapSessionData.communityIndex != communityIndex){
+        [children[referenceIndex], children[communityIndex]] = [children[swapSessionData.referenceIndex], children[swapSessionData.communityIndex]];
+        menu.innerHTML = '';
+    children.forEach(child => menu.appendChild(child));
+    }
+}
+
+if(!swapSessionData){
+    [children[referenceIndex], children[communityIndex]] = [children[communityIndex], children[referenceIndex]];
+    menu.innerHTML = '';
+    children.forEach(child => menu.appendChild(child));
+    
+    const newSwapSession = {
+                                isSwaped:true,
+                                communityIndex: Array.from(menu.children).indexOf(communityElement),
+                                referenceIndex: Array.from(menu.children).indexOf(referenceElement),
+                                createdAt: Date.now()
+                            }
+
+sessionStorage.setItem('swapSession', JSON.stringify(newSwapSession));
+}
